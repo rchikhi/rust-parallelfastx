ddcab15f864b4d4118d5176d7cbfa48fbbbb848c
Rayan  CHIKHI <rchikhi@maestro-2499.maestro.pasteur.fr>
2022-10-09 21:44:38 +0200

    added robustness for small files

--- src/lib.rs
+++ src/lib.rs
@@ -13,16 +13,31 @@ pub fn parallel_fastx<F>(filename: &str, nb_threads: usize, task: F)
     let mut start_pos : Vec<usize> = Vec::new();
     let mut end_pos : Vec<usize> = Vec::new();
     let mut mmap_chunk = Vec::new();
+    let mut big_enough_file = true;
     start_pos.push(0);
     for i in 1..nb_threads
     {
 	let mut start = i*len_file/nb_threads;
 	// adjust starting position of chunk to align with fastq or fasta 
-	while ! ((mmap[start] == '@' as u8 || mmap[start] == '>' as u8) && (mmap[start-1] == '\n' as u8 || mmap[start-1] == '\r' as u8)) { start += 1};
+	while (start < len_file) && (! ((mmap[start] == '@' as u8 || mmap[start] == '>' as u8) && (mmap[start-1] == '\n' as u8 || mmap[start-1] == '\r' as u8))) { start += 1};
+        if start >= len_file - 1 { big_enough_file = false; };
 	end_pos.push(start-1);
 	start_pos.push(start);
     }
     end_pos.push(len_file-1);
+    for i in 1..nb_threads{
+        if start_pos[i] == start_pos[i-1] {
+            big_enough_file = false;
+            break;
+        }
+    }
+    if ! big_enough_file {
+        println!("File too 'small' to be parsed using rust-parallelfastx. Falling back to 1 thread.");
+        start_pos[0] = 0;
+        end_pos[0] = len_file;
+    }
+    let nb_threads = if big_enough_file { nb_threads } else {1};
+
     for i in 0..nb_threads {
 	mmap_chunk.push(mmap[start_pos[i]..end_pos[i]].as_ptr() as usize);
     }
