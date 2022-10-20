db839e9d06d96476a7a384e1815c822a3e5b5df8
Rayan  CHIKHI <rchikhi@maestro-submit.maestro.pasteur.fr>
2022-10-20 12:03:59 +0200

    removed black_box as it requires nightly

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#![feature(bench_black_box)]
 use std::time::Instant;
 use rust_parallelfastx::parallel_fastx;
 
@@ -8,7 +7,6 @@ fn main() {
     let nb_threads = args[2].parse::<usize>().unwrap();
     let dummy = |_seq_str :&[u8], _seq_id: &str| { 
         //println!("{}",_seq_id.to_string()); 
-        std::hint::black_box(1); 
     };
     println!("Parsing input FASTX file {} ({} threads)", filename, nb_threads);
     let start = Instant::now();
